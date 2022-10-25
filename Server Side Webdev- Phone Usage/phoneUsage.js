3cdee17b178870b3bc7209b9a600cb53506c20a0
Martin Swift <76755760+Mintboi@users.noreply.github.com>
2022-10-25 14:53:26 +0100

    Add files via upload

---
+++ Server Side Webdev- Phone Usage/phoneUsage.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+var phoneUsageSchemaful = new Schema({
+    education: {
+        type: Number,
+        required: true
+    },
+    browsing: {
+        type: Number,
+        required: true
+    },
+    shopping: {
+        type: Number,
+        required: true
+        }
+    },
+    {
+    timestamps: true
+});
+var phones = mongoose.model('phones', phoneUsageSchemaful);
+
+module.exports = phones;
